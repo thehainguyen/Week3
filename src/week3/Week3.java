b74e021f09f4cd3a3fd819cd39b92ff6844604dc
The Hai Nguyen <nguytheh@sheridancollege.ca>
2024-09-18 12:21:26 -0400

    Update Week3.java to print Hello World

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -15,7 +15,8 @@ public class Week3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Hello");
+        System.out.print("Hello ");
+        System.out.print("World");
     }
     
 }
